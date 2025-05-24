9d389f0b780a12432a87567702107d5a86b4d1e8
asapha <git@asapha.me>
2025-05-24 16:56:52 +0200

    app works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,10 +18,19 @@ fun App() {
   var text by remember { mutableStateOf("Hello, World!") }
 
   MaterialTheme {
-    Button(onClick = {
-      text = "Hello, Desktop!"
-    }) {
-      Text(text)
+    Column {
+      Button(onClick = {
+        text = "Hello, Desktop!"
+      }) {
+        Text(text)
+      }
+
+      var value by remember { mutableStateOf("Hello, World!") }
+      Text(value)
+      Button(onClick = {}, enabled = true) {
+        Text("Hey")
+      }
+      OutlinedTextField(value, onValueChange = { value = it })
     }
   }
 }
